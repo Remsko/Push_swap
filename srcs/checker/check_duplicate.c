5cd330706eb90d3c4f3bc75f69060980005638c4
Remi PINOIT <rpinoit@e2r10p14.42.fr>
2018-05-07 18:11:14 +0200

    duplicate check good

--- srcs/checker/check_duplicate.c
+++ srcs/checker/check_duplicate.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/07 16:25:32 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/05/07 17:53:13 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/05/07 18:10:37 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 void	check_duplicate(t_env *e)
 {
-	(void)e;
+	int	i;
+	int j;
+
+	i = 0;
+	while (i < e->elem_nb)
+	{
+		j = 0;
+		while (j < e->elem_nb)
+		{
+			if (j != i && e->a[i] == e->a[j]) 
+			{
+				free(e->a);
+				free(e->b);
+				ft_error(6);
+			}
+			j++;
+		}
+		i++;
+	}
 }
