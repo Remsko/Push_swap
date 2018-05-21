024cd494137e9327b6162bd98e6fd1bd4c2ed5c7
Remi PINOIT <rpinoit@e2r12p11.42.fr>
2018-05-21 17:47:27 +0200

    recursive quicksort up

--- srcs/push_swap/main.c
+++ srcs/push_swap/main.c
@@ -6,7 +6,7 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/11 09:44:17 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/05/15 21:44:34 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/05/21 17:41:55 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,7 @@ int     main(int argc, char **argv)
         e = (t_env){.argc = argc, .argv = argv, .elem_nb = 0, .a_len = 0,
                     .b_len = 0, .print = TRUE, .lst = NULL, .a = NULL, .b = NULL};
         parse_params(&e);
-		//operations_algo(&e);
-        quicksort(&e);
+        recursive_quicksort(&e, e.elem_nb, 'a');
     }
     else
         ft_putendl("usage: ./push_swap + integer argument(s)");
--- srcs/push_swap/quicksort.c
+++ srcs/push_swap/quicksort.c
@@ -6,35 +6,69 @@
 /*   By: rpinoit <rpinoit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/15 15:25:39 by rpinoit           #+#    #+#             */
-/*   Updated: 2018/05/15 21:59:41 by rpinoit          ###   ########.fr       */
+/*   Updated: 2018/05/21 17:47:04 by rpinoit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incs/push_swap.h"
 
-void	recursive_quicksort(t_env *e)
-{
-	int pivot;
-	int op_nb;
+#define PILE_LEN (pile == 'a' ? e->a_len : e->b_len)
+#define PILE_CHOSE (pile == 'a' ? e->a : e->b)
+#define PILE_REVERSE (pile == 'a' ? 'b' : 'a')
+#define PILE_FIRST (pile == 'a' ? e->a[0] : e->b[0])
 
-	pivot = e->elem_nb - e->a_len / 2;
-	op_nb = 0;
-	while (op_nb < e->a_len)
+int		quicksort_to(t_env *e, char pile, int op_nb, int pivot)
+{
+	int pushed;
+
+	pushed = 0;
+	while (op_nb--)
 	{
-		if (e->a[0] <= pivot)
-			pb(e);
+		if (PILE_FIRST <= pivot)
+		{
+			pile == 'a' ? pb(e) : pa(e);
+			++pushed;
+		}
 		else
-			ra(e);
-		++op_nb;
+			pile == 'a' ? ra(e) : rb(e);
 	}
-	if (pivot >= e->elem_nb)
-		return ;
-	return (recursive_quicksort(e));
+	return (pushed); 
 }
 
-void	quicksort(t_env *e)
+int		get_median(int *actual, int len)
 {
-	if (issorted(e) == TRUE)
+	int i;
+	int min;
+	int max;
+
+	max = 0;
+	min = 2147483647;
+	i = 0;
+	while (i < len)
+	{
+		min = actual[i] < min ? actual[i] : min;
+		max = actual[i] > max ? actual[i] : max;
+		i++;
+	}
+	return ((min + max) / 2);
+}
+
+void	recursive_quicksort(t_env *e, int len, char pile)
+{
+	int pivot;
+	int pushed;
+
+	if (issorted(e) == TRUE || len <= 0 || e == NULL)
 		return ;
-	recursive_quicksort(e);
+	pivot = get_median(PILE_CHOSE, len);
+	pushed = quicksort_to(e, pile, len, pivot);
+
+	if (pushed > 0 && pile == 'b')
+		recursive_quicksort(e, pushed, 'a');
+	if (pushed <= 5)
+		return ;
+	else
+		recursive_quicksort(e, len - pushed, pile);
+	if (pushed > 0 && pile == 'a')
+		recursive_quicksort(e, pushed, 'b');
 }
